7057d7515e00fb37936952da07e19f49f6a05994
Paulina Rutkowska <polciaalicious@gmail.com>
2020-02-04 16:19:27 +0100

    third

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const age = 30;
 console.log(`Nazywam sie ${name} i mam ${age} lat.`)
 
 console.log('Powodzenia w dalszej pracy')
+
+const mainHeader = document.querySelector ('.mainHeader');
+
+console.log(mainHeader.innerHTML);
+
+mainHeader.innerHTML = '<strong>Borboletas</strong> motyle sryleS'
