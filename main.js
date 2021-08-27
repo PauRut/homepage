a6a61cf71b460e68d676117041f8320ecf4d0731
Paulina Rutkowska <polciaalicious@gmail.com>
2021-08-27 10:49:35 +0200

    getignore definition

--- main.js
+++ main.js
@@ -1,12 +1,90 @@
-console.log ('hola');
-const name = 'Paulina';
-const age = 30;
-console.log(`Nazywam sie ${name} i mam ${age} lat.`)
+const firstName = 'Paulina';
+const age = 32;
 
-console.log('Powodzenia w dalszej pracy')
+const humanOne = {
+    name: 'Maciek',
+    age: 25,
+    address: {
+        street: 'Warszawa',
+        city: 'Bialystok'
+    }
+}
 
-const mainHeader = document.querySelector ('.mainHeader');
+const humanTwo = {
+    name: 'Stefan',
+    age: 32,
+    address: {
+        street: humanOne.address.street,
+        city: 'Bialystok'
+    }
+}
 
-console.log(mainHeader.innerHTML);
 
-mainHeader.innerHTML = '<strong>Borboletas</strong> motyle sryleS'
+if (humanOne.age > humanTwo.age) {
+    console.log('Human One jest starczy');
+} else if (humanOne.age === humanTwo.age) {
+    console.log('Human Two i Human One lub sa rowiesnikami');
+} else {
+    console.log('Human Two jest starszy');
+}
+
+
+const myNumber = 8;
+
+switch (myNumber) {
+    case 7:
+        console.log('jestem 7');
+        break;
+    case 9:
+        console.log('jestem dziewiatka');
+        break;
+    default:
+        console.log('jestem czyms innym');
+}
+
+
+const button = document.querySelector('.action--js');
+console.log(button)
+
+
+button.addEventListener('click', () => {
+    const heading = document.querySelector(".header__title--js")
+    heading.innerHTML = 'klik, klik';
+});
+
+
+const photo = document.querySelector('.about__picture');
+
+photo.addEventListener('mouseenter', () => {
+    const heading = document.querySelector(".about__header")
+    heading.innerHTML = 'siemka';
+});
+
+
+
+
+
+
+
+
+
+
+// function createContent(querySelectorContent, content) {
+//     const element = document.querySelector('.week-summary__description--js');
+//     element.innerHTML = `Nawet uzupelniam tresc javascriptem!`;
+// }
+
+// createContent('week-summary__description--js')
+
+// const mainHeader = document.querySelector ('.mainHeader');
+
+
+
+// const button = document.querySelector('.header__button--js');
+// console.log(button)
+
+// button.addEventListener('click',(e) => {
+//     const header = document.querySelector ('.header__title--js');
+//     header.innerHTML='klik, klik';
+//     header.classList.toggle('header__title--red');
+// });/*
